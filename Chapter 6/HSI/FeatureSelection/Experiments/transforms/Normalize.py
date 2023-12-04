2a01767c855ca690e1e59c4feb02a0cdcc01a4ae
Abian Hernandez <abianhernandez@outlook.es>
2023-12-04 15:45:11 +0000

    Fixing error in inverse_transform()

---
+++ Chapter 6/HSI/FeatureSelection/Experiments/transforms/Normalize.py
@@ -0,0 +1,16 @@
+import torch
+
+class Normalize(torch.nn.Module):
+    def __init__(self, mean, std) -> None:
+        super(Normalize, self).__init__()
+        self.mean = torch.tensor(mean).float()
+        self.std = torch.tensor(std).float()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return (x - self.mean) / self.std
+
+    def inverse_transform(self, x: torch.Tensor) -> torch.Tensor:
+        std_inv = 1 / (self.std + 1e-16)
+        mean_inv = -self.mean * std_inv
+
+        return (x - mean_inv) / std_inv
